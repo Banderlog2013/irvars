716f9017d5f13d341eae2fefbfb020abc45b1bdc
Pavel <banderlog2013@yandex.ru>
2022-10-30 02:57:32 +0300

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import "./slider"
 import modals from './modules/modals' // подключаем modals.js
+import tabs from './modules/tabs'
 
 window.addEventListener('DOMContentLoaded', () => {// назначаем обработчик на весь документ
     modals(); // вызываем переменную
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,44 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    // Получаем элементы со страницы
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector);
+
+    // Ф-я скрытия контента
+    function hideTabContent () {
+        content.forEach(item => { // перебираем элементы
+            item.style.display = 'none'; // скрываем элементы
+        });
+
+        // убираем класс активности у табов
+        tab.forEach(item => {
+            item.classList.remove(activeClass)
+        });
+    }
+
+    // Ф-я показа контента
+    function showTabContent(i = 0) {
+        content[i].style.display ='block';
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    // Отслеживание в какой таб кликнул пользователь
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && 
+            (target.classList.contains(tabSelector.replace(/\./, "")) ||
+        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
